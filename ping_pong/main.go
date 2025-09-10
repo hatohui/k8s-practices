ab937aacb74d546af0257223e26caf73b7c0862a
Lê Sỹ Tuyền <hatohuidev@gmail.com>
2025-09-10 16:08:38 +0700

    feat: update ping-pong service and deployment configurations; add resource limits and new ingress path

---
+++ ping_pong/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	var count = 0
+	
+	router.GET("/ping", func(c *gin.Context) {
+		count++
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong " + fmt.Sprint(count),	
+		})
+	})
+
+	router.Run(":8085")
+}
